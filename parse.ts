bc234225640060c59d74426ac91be2191418e4fe
0aoq <73413330+0aoq@users.noreply.github.com>
2021-06-13 21:54:22 -0400

    Update parse.ts

--- parse.ts
+++ parse.ts
@@ -33,6 +33,28 @@ http.createServer(function (req, res) {
     res.end();
 }).listen(8080);
 
+let variables = []
+
+const $check = function(expr){
+    const holder = []
+    const openBrackets = ['(','{','[']
+    const closedBrackets = [')','}',']']
+    for (let letter of expr) { // loop trought all letters of expr
+        if(openBrackets.includes(letter)){ // if its oppening bracket
+            holder.push(letter)
+        }else if(closedBrackets.includes(letter)){ // if its closing
+            const openPair = openBrackets[closedBrackets.indexOf(letter)] // find its pair
+            if(holder[holder.length - 1] === openPair){ // check if that pair is the last element in the array
+                holder.splice(-1,1) // if so, remove it
+            }else{ // if its not
+                holder.push(letter)
+                break // exit loop
+            }
+        }
+    }
+    return (holder.length === 0) // return true if length is 0, otherwise false
+}
+
 const writeReturnErr = function(fileName: string, newdata: string, add: boolean) {
     fs.readFile(fileName, 'utf8', function (err, data) {
         /* if (err) {
@@ -50,6 +72,10 @@ const writeReturnErr = function(fileName: string, newdata: string, add: boolean)
     })
 }
 
+async function sleep(sec) {
+    return new Promise(resolve => setTimeout(resolve, sec * 1000));
+}
+
 const getArgs = function(cmd: string, limit: number, split: number) {
     let $ = cmd.split(" ", limit)
     $ = cmd.split($[split])
@@ -58,13 +84,31 @@ const getArgs = function(cmd: string, limit: number, split: number) {
     return returned
 }
 
+const getVariable = function($name: string) {
+    for (let variable of variables) {
+        if (variable.name == $name) {
+            return variable
+        }
+    }
+}
+
 const handleCommand = function(cmd: string) {
     let $ = cmd.split(" ")
     if ($) {
+        if ($[0] == "log") {
+            $ = cmd.split($[0])
+            let returned = $[1].slice(1)
+            
+            if (getVariable(returned) != null) {
+                console.log(getVariable(returned).val)
+            } else {
+                console.log(returned)
+            }
+        } 
         // ===============
         // FILE SYSTEM
         // ===============
-        if ($[0] == "dirname") {
+        else if ($[0] == "dirname") {
             console.log(__dirname)
         } else if ($[0] == "read") {
             $ = cmd.split($[0])
@@ -91,7 +135,7 @@ const handleCommand = function(cmd: string) {
             let $data = getArgs(cmd, 2, 1)
 
             writeReturnErr($name, $data, true)
-        } else if ($[0] == "rm_file") {
+        } else if ($[0] == "rm") {
             try {
                 fs.unlinkSync(getArgs(cmd, 2, 0));
             } catch (err) {
@@ -109,14 +153,86 @@ const handleCommand = function(cmd: string) {
 
                 console.log("Open localhost//listdir for more information on this command.")
             });
-        } else if ($[0] == "mk_file") {
-            fs.writeFile(getArgs(cmd, 2, 0), "")
+        } else if ($[0] == "mk") {
+            if (getArgs(cmd, 2, 0) == "includedir") {
+                fs.mkdir(path.join(__dirname, cmd.split(" ")[1].split("/")[0]), (err) => {
+                    if (err) {
+                        return console.error(err);
+                    }
+                });
+            }
+
+            fs.writeFile(getArgs(cmd, 2, 1), "", function(err) {
+                if (err) {
+                    console.log("Created.")
+                }
+            })
+        } else if ($[0] == "exec") {
+            $ = cmd.split($[0])
+            let returned = $[1].slice(1)
+
+            if (returned.split(".")[1] == "0a") {
+                fs.readFile(returned, 'utf8', function (err, data) {
+                    if (err) {
+                        console.log("Recent command returned an error.")
+                    }
+    
+                    // split the contents by new line
+                    const lines = data.split(/\r?\n/);
+    
+                    // print all lines
+                    lines.forEach((line) => {
+                        handleCommand(line)
+                    });
+                })
+            } else {
+                console.log("> Error: file must be a .0a file!")
+            }
+        }
+        // ===============
+        // UTILITY
+        // ===============
+        else if ($[0] == "val") {
+            let $name = getArgs(cmd, 2, 0)
+            let $value = $name.split(" = ")[1]
+
+            if (getVariable('val:' + $name.split(" = ")[0]) == null) {
+                variables.push(
+                    {
+                        name: 'val:' + $name.split(" = ")[0],
+                        val: $value
+                    }
+                )
+            } else {
+                console.log(`> Syntax error: variable has already been declared. Please set it with "${$name.split(" = ")[0]} = ${$value}"`)
+            }
+        } else if ($[0] == "repeat") {
+            $ = cmd.split(" ")
+            let returned = getArgs(cmd, 2, 0).split(" => wait ")
+            let $do = returned[0]
+            let $every = returned[1]
+
+            async function worker() {
+                var i
+                for (i = 0; i < $every; i++) {
+                    handleCommand($do)
+                    await sleep($every)
+                } 
+            }
+
+            worker()
         }
         // ===============
         // IF NO CMD
         // ===============
         else {
-            console.log("> Command not recognized.")
+            let returned = cmd.split(" ")[0]
+            
+            if (getVariable('val:' + returned) != null) {
+                getVariable('val:' + returned).val = getArgs(cmd, 1, 0).split("= ")[1]
+            } else {
+                console.log("> Command not recognized.")
+            }
         }
     }
 }

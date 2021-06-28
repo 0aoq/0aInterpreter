958c58e68b90ce176aa2fb0db096380645a365df
0aoq <73413330+0aoq@users.noreply.github.com>
2021-06-28 19:13:28 -0400

    Update parse.ts

--- parse.ts
+++ parse.ts
@@ -1,29 +1,33 @@
-// npx tsc parse.ts
-// node parse.js
+// parse.ts - main interpreter file
 
 /*
-    0A PROGRAMMING LANGUAGE
+# 0a Interpreter
 
-    - Author: [0a_oq](https://github.com/0aoq)
-    - Version: 0.5.2
+- Author: [0a_oq](https://github.com/0aoq)
+- Version: 0.5.2
 
-    # About
-    A very simple node command line basic programming language.
+## About
 
-    ## Currently Includes
-    Multi-line parsing engine,
-    Basic commands for system management
-    Powerful utility commands
+A very simple node command line basic programming language.
 
-    # Links
-    Sourced at: https://github.com/0aoq/0aInterpreter
-    Documentation at: https://github.com/0aoq/0aDocumentation
+### Currently Includes
 
-    # License
+- Multi-line parsing engine,
+- Basic commands for system management
+- Powerful utility commands
 
-    The 0a programming language is licensed under the **GNU Lesser General Public License v2.1** license.
-    For more information of limits and permissions view [the license page](https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html).
-    https://github.com/0aoq/0aInterpreter/blob/main/LICENSE
+## Links
+
+Sourced at: https://github.com/0aoq/0aInterpreter
+
+Documentation at: https://github.com/0aoq/0aDocumentation
+
+## License
+
+The 0a programming language is licensed under the **GNU Lesser General Public License v2.1** license.
+For more information of limits and permissions view [the license page](https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html).
+
+https://github.com/0aoq/0aInterpreter/blob/main/LICENSE
 */
 
 const readline = require('readline')

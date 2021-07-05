d6155d14718418e3fcf765e25f6fa074d9a85bda
0aoq <73413330+0aoq@users.noreply.github.com>
2021-07-05 11:56:29 -0400

    Add files via upload

---
+++ package/exports/custom/log.ts
@@ -0,0 +1,33 @@
+import { createCmdFromFile, handleCommand } from '../../index.js';
+import { $checkBrackets, $checkQuotes, getArgs, getVariable, parseFunction, parseString } from '../utility.js';
+
+createCmdFromFile("log", false, function ($) {
+    // really long error handling section
+    if (parseFunction(getArgs($.cmd, 2, 0))[0] == '"') { // is a string
+        if ($checkBrackets($.cmd.slice(4))) {
+            if ($checkQuotes(parseFunction(getArgs($.cmd, 2, 0)))) {
+                console.log(parseString(parseFunction(getArgs($.cmd, 2, 0))))
+            } else {
+                handleCommand("SyntaxError quotes weren't closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
+            }
+        } else {
+            handleCommand("SyntaxError brakets were not closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
+        }
+    } else if (getVariable(parseFunction(getArgs($.cmd, 2, 0))) || !isNaN(parseInt(parseFunction(getArgs($.cmd, 2, 0))))) { // is a variable/int
+        if ($checkBrackets($.cmd.slice(4))) {
+            if (getVariable(parseFunction(getArgs($.cmd, 2, 0)))) { // specific for variable
+                console.log(getVariable(parseFunction(getArgs($.cmd, 2, 0))).val)
+            } else {
+                if (!isNaN(parseInt(parseFunction(getArgs($.cmd, 2, 0))))) { // number
+                    console.log(parseInt(parseFunction(getArgs($.cmd, 2, 0))))
+                } else { // any
+                    console.log(parseFunction(getArgs($.cmd, 2, 0)))
+                }
+            }
+        } else {
+            handleCommand("SyntaxError brakets were not closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
+        }
+    } else {
+        handleCommand("SyntaxError log type not recognized.", $.callingFrom, $.addToVariables, $.line)
+    }
+})

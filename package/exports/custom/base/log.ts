354db4cb9783479ca142817534c1c44b6df13e2e
0aoq <a0oq@outlook.com>
2021-07-12 14:41:42 -0400

    Log command error handling fixed

--- package/exports/custom/base/log.ts
+++ package/exports/custom/base/log.ts
@@ -6,42 +6,46 @@ createCmdFromFile("log", false, function ($) {
     let after = getLineAfterCmd($.cmd, "log")
     
     // really long error handling section
-    if (parseFunction(after)[0] === '"') { // is a string
-        if ($checkBrackets(after)) {
-            if ($checkQuotes(parseFunction(after))) {
-                console.log(parseString(parseFunction(after)))
-            } else {
-                handleCommand("SyntaxError quotes weren't closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
-            }
-        } else {
-            handleCommand("SyntaxError brakets were not closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
-        }
-    } else if (parseFunction(after)[0] == "{") {
-        if ($checkBrackets(after)) {
-            console.log(JSON.parse(parseFunction(after)))
-        } else {
-            handleCommand("SyntaxError brakets were not closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
-        }
-    } else if (getVariable(parseFunction(after)) || !isNaN(parseInt(parseFunction(after)))) { // is a variable/int
-        if ($checkBrackets(after)) {
-            if (getVariable(parseFunction(after))) { // specific for variable
-                let variable = getVariable(parseFunction(after))
-                if (variable.__type == "table") {
-                    console.log(JSON.parse(variable.absoluteValue)) // parse to table
+    if (parseFunction(after) && $checkBrackets(after)) {
+        if (parseFunction(after)[0] === '"') { // is a string
+            if ($checkBrackets(after)) {
+                if ($checkQuotes(parseFunction(after))) {
+                    console.log(parseString(parseFunction(after)))
                 } else {
-                    console.log(variable.absoluteValue)
+                    handleCommand("SyntaxError quotes weren't closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
                 }
             } else {
-                if (!isNaN(parseInt(parseFunction(after)))) { // number
-                    console.log(parseInt(parseFunction(after)))
-                } else { // any
-                    console.log(parseFunction(after))
+                handleCommand("SyntaxError brakets were not closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
+            }
+        } else if (parseFunction(after)[0] == "{") {
+            if ($checkBrackets(after)) {
+                console.log(JSON.parse(parseFunction(after)))
+            } else {
+                handleCommand("SyntaxError brakets were not closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
+            }
+        } else if (getVariable(parseFunction(after)) || !isNaN(parseInt(parseFunction(after)))) { // is a variable/int
+            if ($checkBrackets(after)) {
+                if (getVariable(parseFunction(after))) { // specific for variable
+                    let variable = getVariable(parseFunction(after))
+                    if (variable.__type == "table") {
+                        console.log(JSON.parse(variable.absoluteValue)) // parse to table
+                    } else {
+                        console.log(variable.absoluteValue)
+                    }
+                } else {
+                    if (!isNaN(parseInt(parseFunction(after)))) { // number
+                        console.log(parseInt(parseFunction(after)))
+                    } else { // any
+                        console.log(parseFunction(after))
+                    }
                 }
+            } else {
+                handleCommand("SyntaxError brakets were not closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
             }
         } else {
-            handleCommand("SyntaxError brakets were not closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
+            handleCommand("SyntaxError log type not recognized.", $.callingFrom, $.addToVariables, $.line)
         }
     } else {
-        handleCommand("SyntaxError log type not recognized.", $.callingFrom, $.addToVariables, $.line)
+        handleCommand("SyntaxError brakets were not closed properly for log function.", $.callingFrom, $.addToVariables, $.line)
     }
 })
